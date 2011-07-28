c86b0723aff93d12983b7ec25336e0cd8c2e1868
Andrew Cholakian <andrew@andrewvc.com>
2011-07-28 08:10:29 -0700

    Temporarily remove Numeric monkey patching

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -105,16 +105,16 @@ module Clockwork
 	end
 end
 
-class Numeric
-	def seconds; self; end
-	alias :second :seconds
-
-	def minutes; self * 60; end
-	alias :minute :minutes
-
-	def hours; self * 3600; end
-	alias :hour :hours
-
-	def days; self * 86400; end
-	alias :day :days
-end
+#class Numeric
+#	def seconds; self; end
+#	alias :second :seconds
+#
+#	def minutes; self * 60; end
+#	alias :minute :minutes
+#
+#	def hours; self * 3600; end
+#	alias :hour :hours
+#
+#	def days; self * 86400; end
+#	alias :day :days
+#end
